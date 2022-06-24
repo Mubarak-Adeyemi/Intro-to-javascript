9b4d39f6855c8dde3bf6013019a1c310e8954ccb
Mubarak Adeyemi <hardex540@gmail.com>
2022-06-24 22:17:57 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function printName() {
     alert('Name: Dhirullah Mubarak Adeyemi \nHeight: 5ft \nCountry: Nigeria  ')
 }
+printName();
 
 
 document.write('<h1>Name: Dhikrullah Mubarak Adeyemi <br> Height: 5ft <br> Country: Nigeria</h1>');
